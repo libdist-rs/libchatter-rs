71232c400fdf77e2f2a4fe29d8c13c2b4c411fe5
hermitsage <dth.bht@gmail.com>
2020-12-25 14:43:54 -0500

    Add missing echo-exp

---
+++ examples/echo-exp/echo/src/main.rs
@@ -0,0 +1,42 @@
+use clap::{load_yaml, App};
+use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::{TcpListener}};
+use std::{error::Error};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let yaml = load_yaml!("cli.yml");
+    let m = App::from_yaml(yaml).get_matches();
+
+    let port: u16 = m.value_of("port")
+        .expect("please provide port number")
+        .parse()
+        .expect("please provide a valid port number");
+
+    let sock = TcpListener::bind(format!("0.0.0.0:{}",port))
+        .await
+        .expect("Failed to bind to port");
+    let mut buf = [0 as u8; 100_000];
+    loop {
+        let conn = sock.accept().await;
+        if let Err(e) = conn {
+            println!("Error connecting: {}", e);
+            break;
+        }
+        let (mut stream, from) = conn.unwrap();
+        println!("Connected to a sink: {}", from);
+        loop {
+            let x = stream.read(&mut buf).await;
+            if let Err(e) = x {
+                println!("Closing: {}", e);
+                break;
+            }
+            if let Ok(y) = x{
+                if let Err(e) = stream.write(&mut buf[0..y]).await  {
+                    println!("Closing: {}", e);
+                    break;
+                }
+            }
+        }
+    }
+    Ok(())
+}
