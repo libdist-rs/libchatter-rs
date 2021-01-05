905df47963221208073de9d393752e60b1df2e0b
hermitsage <dth.bht@gmail.com>
2021-01-05 18:09:21 -0500

    add partial logging ability

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -13,10 +13,13 @@ pub mod codec;
 // pub mod channel;
 
 pub fn new_dummy_tx(i:u64, payload:usize) -> Transaction {
-    Transaction{
+    log::trace!(target:"util", "Creating a dummy transaction with payload {}", payload);
+    let t = Transaction{
         data: i.to_be_bytes().to_vec(),
         request: vec![1;payload],
-    }
+    };
+    log::trace!(target:"util", "Created dummy transaction {:?}", t);
+    t
 }
 
 pub const CHANNEL_SIZE:usize = 100_000;
