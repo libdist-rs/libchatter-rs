55eb4f41b915f29bcc8c1666d89039fff2f12bd6
hermitsage <dth.bht@gmail.com>
2021-01-12 23:47:40 -0500

    Add tls based genconfig

--- tools/genconfig/src/main.rs
+++ tools/genconfig/src/main.rs
@@ -4,12 +4,139 @@
 use crypto::{ed25519, secp256k1};
 use config::{Node, Client};
 use clap::{load_yaml, App};
-use types::{Replica};
+use types::Replica;
 use crypto::Algorithm;
-use std::{collections::HashMap};
+use std::{
+    collections::HashMap, 
+    error::Error
+};
 use util::io::*;
+use openssl::{asn1::Asn1Time, bn::{BigNum, MsbOption}, error::ErrorStack, hash::MessageDigest, pkey::{PKey, PKeyRef, Private}, rsa::Rsa, x509::{X509, X509NameBuilder, X509Ref, X509Req, X509ReqBuilder, extension::{AuthorityKeyIdentifier, BasicConstraints, KeyUsage, SubjectAlternativeName, SubjectKeyIdentifier}}};
 
-fn main() {
+fn new_root_cert() -> Result<(X509, PKey<Private>), ErrorStack> {
+    let rsa = Rsa::generate(2048)?;
+    let privkey = PKey::from_rsa(rsa)?;
+
+    let mut x509_name = X509NameBuilder::new()?;
+    x509_name.append_entry_by_text("C", "US")?;
+    x509_name.append_entry_by_text("ST", "IN")?;
+    x509_name.append_entry_by_text("O", "Libchatter Test")?;
+    x509_name.append_entry_by_text("CN", "Root")?;
+    let x509_name = x509_name.build();
+
+    let mut cert_builder = X509::builder()?;
+    cert_builder.set_version(2)?;
+    let serial_number = {
+        let mut serial = BigNum::new()?;
+        serial.rand(159, MsbOption::MAYBE_ZERO, false)?;
+        serial.to_asn1_integer()?
+    };
+    cert_builder.set_serial_number(&serial_number)?;
+    cert_builder.set_subject_name(&x509_name)?;
+    cert_builder.set_issuer_name(&x509_name)?;
+    cert_builder.set_pubkey(&privkey)?;
+    let not_before = Asn1Time::days_from_now(0)?;
+    cert_builder.set_not_before(&not_before)?;
+    let not_after = Asn1Time::days_from_now(365)?;
+    cert_builder.set_not_after(&not_after)?;
+
+    cert_builder.append_extension(BasicConstraints::new().critical().ca().build()?)?;
+    cert_builder.append_extension(
+        KeyUsage::new()
+            .critical()
+            .key_cert_sign()
+            .crl_sign()
+            .build()?,
+    )?;
+
+    let subject_key_identifier =
+        SubjectKeyIdentifier::new().build(&cert_builder.x509v3_context(None, None))?;
+    cert_builder.append_extension(subject_key_identifier)?;
+
+    cert_builder.sign(&privkey, MessageDigest::sha256())?;
+    let cert = cert_builder.build();
+
+    Ok((cert, privkey))
+}
+
+/// Make a X509 request with the given private key
+fn mk_request(privkey: &PKey<Private>) -> Result<X509Req, ErrorStack> {
+    let mut req_builder = X509ReqBuilder::new()?;
+    req_builder.set_pubkey(&privkey)?;
+
+    let mut x509_name = X509NameBuilder::new()?;
+    x509_name.append_entry_by_text("C", "US")?;
+    x509_name.append_entry_by_text("ST", "IN")?;
+    x509_name.append_entry_by_text("O", "Nodes")?;
+    x509_name.append_entry_by_text("CN", "nodes.com")?;
+    let x509_name = x509_name.build();
+    req_builder.set_subject_name(&x509_name)?;
+
+    req_builder.sign(&privkey, MessageDigest::sha256())?;
+    let req = req_builder.build();
+    Ok(req)
+}
+
+/// Make a certificate and private key signed by the given CA cert and private key
+fn get_signed_cert(
+    ca_cert: &X509Ref,
+    ca_privkey: &PKeyRef<Private>,
+) -> Result<(X509, PKey<Private>), ErrorStack> {
+    let rsa = Rsa::generate(2048)?;
+    let privkey = PKey::from_rsa(rsa)?;
+
+    let req = mk_request(&privkey)?;
+
+    let mut cert_builder = X509::builder()?;
+    cert_builder.set_version(2)?;
+    let serial_number = {
+        let mut serial = BigNum::new()?;
+        serial.rand(159, MsbOption::MAYBE_ZERO, false)?;
+        serial.to_asn1_integer()?
+    };
+    cert_builder.set_serial_number(&serial_number)?;
+    cert_builder.set_subject_name(req.subject_name())?;
+    cert_builder.set_issuer_name(ca_cert.subject_name())?;
+    cert_builder.set_pubkey(&privkey)?;
+    let not_before = Asn1Time::days_from_now(0)?;
+    cert_builder.set_not_before(&not_before)?;
+    let not_after = Asn1Time::days_from_now(365)?;
+    cert_builder.set_not_after(&not_after)?;
+
+    cert_builder.append_extension(BasicConstraints::new().build()?)?;
+
+    cert_builder.append_extension(
+        KeyUsage::new()
+            .critical()
+            .non_repudiation()
+            .digital_signature()
+            .key_encipherment()
+            .build()?,
+    )?;
+
+    let subject_key_identifier =
+        SubjectKeyIdentifier::new().build(&cert_builder.x509v3_context(Some(ca_cert), None))?;
+    cert_builder.append_extension(subject_key_identifier)?;
+
+    let auth_key_identifier = AuthorityKeyIdentifier::new()
+        .keyid(false)
+        .issuer(false)
+        .build(&cert_builder.x509v3_context(Some(ca_cert), None))?;
+    cert_builder.append_extension(auth_key_identifier)?;
+
+    let subject_alt_name = SubjectAlternativeName::new()
+    //     .dns("*.example.com")
+        .dns("nodes.com")
+        .build(&cert_builder.x509v3_context(Some(ca_cert), None))?;
+    cert_builder.append_extension(subject_alt_name)?;
+
+    cert_builder.sign(&ca_privkey, MessageDigest::sha256())?;
+    let cert = cert_builder.build();
+
+    Ok((cert, privkey))
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     let yaml = load_yaml!("cli.yml");
     let m = App::from_yaml(yaml).get_matches();
     let num_nodes:usize =  m.value_of("num_nodes")
@@ -60,6 +187,8 @@ fn main() {
     let mut pk = HashMap::new();
     let mut ip = HashMap::new();
 
+    let (cert, privkey) = new_root_cert()?;
+
     for i in 0..num_nodes {
         node.push(Node::new());
 
@@ -92,7 +221,15 @@ fn main() {
         client.net_map.insert(i as Replica, 
         format!("127.0.0.1:{}", client_base_port+(i as u16))
         );
+
+        let (new_cert, new_pkey) = get_signed_cert(&cert, &privkey)?;
+
+        node[i].root_cert = cert.to_der()?;
+        node[i].my_cert = new_cert.to_der()?;
+        node[i].my_cert_key = new_pkey.private_key_to_der()?;
     }
+
+    client.root_cert = cert.to_der()?;
 
     for i in 0..num_nodes {
         node[i].pk_map = pk.clone();
@@ -148,4 +285,18 @@ fn main() {
     }
     client.validate()
         .expect("failed to validate the client config");
+
+    Ok(())
+}
+
+#[test]
+fn test_codec() -> Result<(), Box<dyn Error>>{
+    use rustls::{Certificate, ClientConfig};
+
+    let (cert, _key) = new_root_cert()?;
+    let data = cert.to_der()?;
+    let ok = Certificate(data);
+    let mut config = ClientConfig::new();
+    config.root_store.add(&ok)?;
+    Ok(())
 }
